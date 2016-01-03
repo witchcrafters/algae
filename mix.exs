055b333e371eaea6fa1730d4a0927872011e935e
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-01-03 01:35:15 -0800

    Drop to Elixir 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Algae.Mixfile do
   def project do
     [app: :algae,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
