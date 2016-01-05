385dc9823f7d8f13c437e3fd199f04a29e48e706
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-01-04 22:58:41 -0800

    Rose trees

---
+++ lib/algae/tree/rose.ex
@@ -0,0 +1,17 @@
+defmodule Algae.Tree.Rose do
+  @type t :: %Algae.Tree.Rose{rose: any, tree: [Algae.Tree.Rose.t]}
+  defstruct rose: nil, tree: [] # Remember that `nil` is a value, not bottom
+end
+
+defimpl Witchcraft.Functor, for: Algae.Tree.Rose do
+  def lift(%Rose.Tree.Rose{rose: rose, tree: []}, fun) do
+    %Algae.Tree.Rose{rose: Quark.Curry.curry(fun).(rose)}
+  end
+
+  def lift(%Rose.Tree.Rose{rose: rose, tree: tree}, fun) do
+    %Algae.Tree.Rose{
+      rose: Quark.Curry.curry(fun).(rose),
+      tree: lift(tree, &(lift(&1, fun)))
+    }
+  end
+end
