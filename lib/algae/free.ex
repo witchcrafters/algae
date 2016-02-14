70ca00039ba1c2e20d09d093b0ecdc8acfdd0a96
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-02-13 20:50:38 -0800

    Clarifying note in %Free{}

--- lib/algae/free.ex
+++ lib/algae/free.ex
@@ -10,6 +10,17 @@ defmodule Algae.Free do
 
   defmodule Wrap do
     @type t :: %Algae.Free.Wrap{wrap: any}
+
+    @moduledoc ~S"""
+    `wrap` must be some functor wrapping another `%Free.X{}`
+
+    ```elixir
+
+    %Free.Wrap{wrap: %Id{id: %Free.Pure{pure: "Terminus"}}
+
+    ```
+
+    """
     defstruct [:wrap]
   end
 
