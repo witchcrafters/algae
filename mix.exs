aa60bfdc7c150805706618bd563aa37d8416cacd
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-08-31 07:08:29 +0900

    Bump version number

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Algae.Mixfile do
      name:    "Algae",
 
      description: "Bootstrapped algebraic data types for Elixir",
-     version: "0.10.0",
+     version: "0.11.0",
      elixir:  "~> 1.3",
 
      package: [
