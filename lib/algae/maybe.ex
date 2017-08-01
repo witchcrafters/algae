5b8c8c34ce1838c4c0031e1db23025c31732eab7
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-01 00:18:32 -0700

    More readable

--- lib/algae/maybe.ex
+++ lib/algae/maybe.ex
@@ -58,3 +58,183 @@ defmodule Algae.Maybe do
   @spec new(any) :: Just.t()
   defdelegate new(value), to: Just, as: :new
 end
+
+import TypeClass
+use Witchcraft
+
+#############
+# Generator #
+#############
+
+defimpl TypeClass.Property.Generator, for: Algae.Maybe.Nothing do
+  def generate(_), do: %Algae.Maybe.Nothing{}
+end
+
+defimpl TypeClass.Property.Generator, for: Algae.Maybe.Just do
+  def generate(_) do
+    [1, 1.1, "", []]
+    |> Enum.random()
+    |> TypeClass.Property.Generator.generate()
+    |> Algae.Maybe.Just.new()
+  end
+end
+
+##########
+# Setoid #
+##########
+
+definst Witchcraft.Setoid, for: Algae.Maybe.Nothing do
+  def equivalent?(_, %Algae.Maybe.Nothing{}), do: true
+  def equivalent?(_, %Algae.Maybe.Just{}),    do: false
+end
+
+definst Witchcraft.Setoid, for: Algae.Maybe.Just do
+  def equivalent?(%{just: a}, %{just: b}), do: a == b
+  def equivalent?(%{just: a}, %Algae.Maybe.Nothing{}), do: false
+end
+
+#######
+# Ord #
+#######
+
+definst Witchcraft.Ord, for: Algae.Maybe.Nothing do
+  def compare(_, %Algae.Maybe.Nothing{}), do: :equal
+  def compare(_, %Algae.Maybe.Just{}),    do: :lesser
+end
+
+definst Witchcraft.Ord, for: Algae.Maybe.Just do
+  custom_generator(_) do
+    1
+    |> TypeClass.Property.Generator.generate()
+    |> Algae.Maybe.Just.new()
+  end
+
+  def compare(%{just: a}, %{just: b}), do: Witchcraft.Ord.compare(a, b)
+  def compare(%{just: a}, %Algae.Maybe.Nothing{}), do: :greater
+end
+
+#############
+# Semigroup #
+#############
+
+definst Witchcraft.Semigroup, for: Algae.Maybe.Nothing do
+  def append(_, right), do: right
+end
+
+definst Witchcraft.Semigroup, for: Algae.Maybe.Just do
+  custom_generator(_) do
+    1
+    |> TypeClass.Property.Generator.generate()
+    |> Algae.Maybe.Just.new()
+  end
+
+  def append(%{just: a}, %{just: b}), do: %Algae.Maybe.Just{just: a <> b}
+  def append(just = %{just: _}, _), do: just
+end
+
+##########
+# Monoid #
+##########
+
+definst Witchcraft.Monoid, for: Algae.Maybe.Nothing do
+  def empty(nothing), do: nothing
+end
+
+definst Witchcraft.Monoid, for: Algae.Maybe.Just do
+  def empty(_), do: %Algae.Maybe.Nothing{}
+end
+
+###########
+# Functor #
+###########
+
+definst Witchcraft.Functor, for: Algae.Maybe.Nothing do
+  def map(_, _), do: %Algae.Maybe.Nothing{}
+end
+
+definst Witchcraft.Functor, for: Algae.Maybe.Just do
+  def map(%{just: data}, fun), do: data |> fun.() |> Algae.Maybe.Just.new()
+end
+
+############
+# Foldable #
+############
+
+definst Witchcraft.Foldable, for: Algae.Maybe.Nothing do
+  def right_fold(_, seed, _), do: seed
+end
+
+definst Witchcraft.Foldable, for: Algae.Maybe.Just do
+  def right_fold(%{just: inner}, seed, fun), do: fun.(inner, seed)
+end
+
+###############
+# Traversable #
+###############
+
+definst Witchcraft.Traversable, for: Algae.Maybe.Nothing do
+  def traverse(_, link), do: %Algae.Maybe.Nothing{}
+end
+
+definst Witchcraft.Traversable, for: Algae.Maybe.Just do
+  def traverse(%{just: data}, link) do
+    data
+    |> link.()
+    |> Witchcraft.Functor.map(&Algae.Maybe.Just.new/1)
+  end
+end
+
+#########
+# Apply #
+#########
+
+definst Witchcraft.Apply, for: Algae.Maybe.Nothing do
+  def ap(_, _), do: %Algae.Maybe.Nothing{}
+end
+
+definst Witchcraft.Apply, for: Algae.Maybe.Just do
+  def ap(%{just: fun}, data), do: Witchcraft.Functor.map(data, fun)
+end
+
+###############
+# Applicative #
+###############
+
+definst Witchcraft.Applicative, for: Algae.Maybe.Nothing do
+  def of(_, data), do: %Algae.Maybe.Just{id: data}
+end
+
+definst Witchcraft.Applicative, for: Algae.Maybe.Just do
+  def of(_, data), do: %Algae.Maybe.Just{id: data}
+end
+
+#########
+# Chain #
+#########
+
+definst Witchcraft.Chain, for: Algae.Maybe.Nothing do
+  def chain(_, _), do: %Algae.Maybe.Nothing{}
+end
+
+definst Witchcraft.Chain, for: Algae.Maybe.Just do
+  def chain(%{just: data}, link), do: link.(data)
+end
+
+#########
+# Monad #
+#########
+
+definst Witchcraft.Monad, for: Algae.Maybe.Nothing
+definst Witchcraft.Monad, for: Algae.Maybe.Just
+
+##########
+# Extend #
+##########
+
+definst Witchcraft.Extend, for: Algae.Maybe.Nothing do
+  def nest(_), do: %Algae.Maybe.Nothing{}
+end
+
+definst Witchcraft.Extend, for: Algae.Maybe.Just do
+  def nest(inner), do: Algae.Maybe.Just.new(inner)
+end
