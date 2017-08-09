671505dd63f57dd396cbfd59ca79d22b4850ae60
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-09 00:25:53 -0700

    WIP

--- lib/algae/free.ex
+++ lib/algae/free.ex
@@ -1,126 +1,192 @@
-defmodule Algae.Free do
-  @moduledoc """
-  A "free" structure, similar to lists
+# Need to look at this again! data Free f a = Pure a | Roll (f (Free f a))
 
-  ## Shallow
-  `Shallow` holds a plain value
 
-  ## Deep
 
-  `Deep` holds two values: a value (often a functor) in `deep`, and another
-  `Algae.Free.t` in `deeper`
 
-      %Free.Deep{
-        deep:   %Id{id: 42},
-        deeper: %Free.Shallow{shallow: "Terminus"}
-      }
+# defmodule Algae.Free do
+#   @moduledoc """
+#   A "free" structure, similar to lists
 
-  """
+#   ## Shallow
+#   `Shallow` holds a plain value
 
-  import Algae
-  alias __MODULE__
+#   ## Deep
 
-  defsum do
-    defdata Shallow :: any()
+#   `Deep` holds two values: a value (often a functor) in `deep`, and another
+#   `Algae.Free.t` in `deeper`
 
-    defdata Deep do
-      deep   :: any()
-      deeper :: any() # Algae.Free.t()
-    end
-  end
+#       %Free.Deep{
+#         deep:   %Id{id: 42},
+#         deeper: %Free.Shallow{shallow: "Terminus"}
+#       }
 
-  @doc """
-  Create a `Algae.Free.Shallow` wrapping a single, simple value
+#   """
 
-  ## Examples
+#   import Algae
+#   alias __MODULE__
 
-      iex> new(42)
-      %Algae.Free.Shallow{shallow: 42}
+#   defsum do
+#     defdata Shallow :: any()
 
-  """
-  @spec new(any()) :: t()
-  def new(value), do: Free.Shallow.new(value)
+#     defdata Deep do
+#       deep   :: any()
+#       deeper :: any() # Algae.Free.t()
+#     end
+#   end
 
-  @doc """
-  Add another layer to a free structure
+#   @doc """
+#   Create a `Algae.Free.Shallow` wrapping a single, simple value
 
-  ## Examples
+#   ## Examples
 
-      iex> 13
-      ...> |> new()
-      ...> |> layer(42)
-      %Algae.Free.Deep{
-        deep: 42,
-        deeper: %Algae.Free.Shallow{
-          shallow: 13
-        }
-      }
+#       iex> new(42)
+#       %Algae.Free.Shallow{shallow: 42}
 
-  """
-  @spec layer(t(), any()) :: t()
-  def layer(free, value), do: %Deep{deep: value, deeper: free}
-end
+#   """
+#   @spec new(any()) :: t()
+#   def new(value), do: Free.Shallow.new(value)
 
-alias Algae.Free.{Deep, Shallow}
-import TypeClass
-use Witchcraft
+#   @doc """
+#   Add another layer to a free structure
 
-#############
-# Generator #
-#############
+#   ## Examples
 
-defimpl TypeClass.Property.Generator, for: Shallow do
-  def generate(_) do
-    [1, 1.1, "", []]
-    |> Enum.random()
-    |> TypeClass.Property.Generator.generate()
-    |> Shallow.new()
-  end
-end
+#       iex> 13
+#       ...> |> new()
+#       ...> |> layer(42)
+#       %Algae.Free.Deep{
+#         deep: 42,
+#         deeper: %Algae.Free.Shallow{
+#           shallow: 13
+#         }
+#       }
 
-defimpl TypeClass.Property.Generator, for: Deep do
-  def generate(_) do
-    [1, 1.1, "", []]
-    |> Enum.random()
-    |> TypeClass.Property.Generator.generate()
-    |> Deep.new()
-  end
-end
+#   """
+#   @spec layer(t(), any()) :: t()
+#   def layer(free, value), do: %Deep{deep: value, deeper: free}
+# end
 
-##########
-# Setoid #
-##########
+# alias Algae.Free.{Deep, Shallow}
+# import TypeClass
+# use Witchcraft
 
-definst Witchcraft.Setoid, for: Algae.Free.Shallow do
-  custom_generator(_) do
-    1
-    |> TypeClass.Property.Generator.generate()
-    |> Algae.Free.new()
-  end
+# #############
+# # Generator #
+# #############
 
-  def equivalent?(_, %Deep{}), do: false
+# defimpl TypeClass.Property.Generator, for: Shallow do
+#   def generate(_) do
+#     [1, 1.1, "", []]
+#     |> Enum.random()
+#     |> TypeClass.Property.Generator.generate()
+#     |> Shallow.new()
+#   end
+# end
 
-  def equivalent?(%Shallow{shallow: a}, %Shallow{shallow: b}) do
-    Witchcraft.Setoid.equivalent?(a, b)
-  end
-end
+# defimpl TypeClass.Property.Generator, for: Deep do
+#   def generate(_) do
+#     sample = Enum.random([1, 1.1, "", []])
 
-definst Witchcraft.Setoid, for: Algae.Free.Deep do
-  custom_generator(_) do
-    a = TypeClass.Property.Generator.generate(1)
-    b = TypeClass.Property.Generator.generate(1)
-    c = TypeClass.Property.Generator.generate(1)
+#     a = TypeClass.Property.Generator.generate(sample)
+#     b = TypeClass.Property.Generator.generate(sample)
+#     c = TypeClass.Property.Generator.generate(sample)
 
-    a
-    |> Algae.Free.new()
-    |> Algae.Free.layer(Algae.Free.new(b))
-    |> Algae.Free.layer(Algae.Free.new(c))
-  end
+#     a
+#     |> Algae.Free.new()
+#     |> Algae.Free.layer(Algae.Free.new(b))
+#     |> Algae.Free.layer(Algae.Free.new(c))
+#   end
+# end
 
-  def equivalent?(_, %Shallow{}), do: false
+# ##########
+# # Setoid #
+# ##########
 
-  def equivalent?(%Deep{deep: deep_a, deeper: deeper_a}, %Deep{deep: deep_b, deeper: deeper_b}) do
-    Witchcraft.Setoid.equivalent?(deep_a, deep_b)
-    and Witchcraft.Setoid.equivalent?(deeper_a, deeper_b)
-  end
-end
+# definst Witchcraft.Setoid, for: Algae.Free.Shallow do
+#   custom_generator(_) do
+#     1
+#     |> TypeClass.Property.Generator.generate()
+#     |> Algae.Free.new()
+#   end
+
+#   def equivalent?(_, %Deep{}), do: false
+
+#   def equivalent?(%Shallow{shallow: a}, %Shallow{shallow: b}) do
+#     Witchcraft.Setoid.equivalent?(a, b)
+#   end
+# end
+
+# definst Witchcraft.Setoid, for: Algae.Free.Deep do
+#   custom_generator(_) do
+#     a = TypeClass.Property.Generator.generate(1)
+#     b = TypeClass.Property.Generator.generate(1)
+#     c = TypeClass.Property.Generator.generate(1)
+
+#     a
+#     |> Algae.Free.new()
+#     |> Algae.Free.layer(Algae.Free.new(b))
+#     |> Algae.Free.layer(Algae.Free.new(c))
+#   end
+
+#   def equivalent?(_, %Shallow{}), do: false
+
+#   def equivalent?(%Deep{deep: deep_a, deeper: deeper_a}, %Deep{deep: deep_b, deeper: deeper_b}) do
+#     Witchcraft.Setoid.equivalent?(deep_a, deep_b)
+#     and Witchcraft.Setoid.equivalent?(deeper_a, deeper_b)
+#   end
+# end
+
+# #######
+# # Ord #
+# #######
+
+# definst Witchcraft.Ord, for: Algae.Free.Shallow do
+#   custom_generator(_) do
+#     1
+#     |> TypeClass.Property.Generator.generate()
+#     |> Algae.Free.new()
+#   end
+
+#   def compare(_, %Deep{}), do: :lesser
+#   def compare(%Shallow{shallow: a}, %Shallow{shallow: b}), do: Witchcraft.Ord.compare(a, b)
+# end
+
+# definst Witchcraft.Ord, for: Algae.Free.Deep do
+#   custom_generator(_) do
+#     a = TypeClass.Property.Generator.generate(1)
+#     b = TypeClass.Property.Generator.generate(1)
+#     c = TypeClass.Property.Generator.generate(1)
+
+#     a
+#     |> Algae.Free.new()
+#     |> Algae.Free.layer(Algae.Free.new(b))
+#     |> Algae.Free.layer(Algae.Free.new(c))
+#   end
+
+#   def compare(%Deep{}, %Shallow{}), do: :greater
+#   def compare(%Deep{deep: deep_a, deeper: deeper_a}, %Deep{deep: deep_b, deeper: deeper_b}) do
+#     case Witchcraft.Ord.compare(deep_a, deep_b) do
+#       :equal -> Witchcraft.Ord.compare(deeper_a, deeper_b)
+#       result -> result
+#     end
+#   end
+# end
+
+# ###########
+# # Functor #
+# ###########
+
+# definst Witchcraft.Functor, for: Algae.Free.Shallow do
+#   def map(%Shallow{shallow: data}, fun), do: Shallow.new(data ~> fun)
+# end
+
+# definst Witchcraft.Functor, for: Algae.Free.Deep do
+#   # fmap f (Roll x) = Roll (fmap (fmap f) x)
+#   def map(deep = %Deep{deep: data}, fun) do
+#     data
+#     |> map(fn x -> map(fun, x) end)
+#     |>
+
+#     %{deep | deep: map(data, fn x -> map(fun, x) end )}
+#   end
+# end
