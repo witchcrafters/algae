6867137ef1de97b559fe098261f37066a677a3ae
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-09 00:06:20 -0700

    Slowly but surely

--- lib/algae/free.ex
+++ lib/algae/free.ex
@@ -92,6 +92,12 @@ end
 ##########
 
 definst Witchcraft.Setoid, for: Algae.Free.Shallow do
+  custom_generator(_) do
+    1
+    |> TypeClass.Property.Generator.generate()
+    |> Algae.Free.new()
+  end
+
   def equivalent?(_, %Deep{}), do: false
 
   def equivalent?(%Shallow{shallow: a}, %Shallow{shallow: b}) do
@@ -100,6 +106,17 @@ definst Witchcraft.Setoid, for: Algae.Free.Shallow do
 end
 
 definst Witchcraft.Setoid, for: Algae.Free.Deep do
+  custom_generator(_) do
+    a = TypeClass.Property.Generator.generate(1)
+    b = TypeClass.Property.Generator.generate(1)
+    c = TypeClass.Property.Generator.generate(1)
+
+    a
+    |> Algae.Free.new()
+    |> Algae.Free.layer(Algae.Free.new(b))
+    |> Algae.Free.layer(Algae.Free.new(c))
+  end
+
   def equivalent?(_, %Shallow{}), do: false
 
   def equivalent?(%Deep{deep: deep_a, deeper: deeper_a}, %Deep{deep: deep_b, deeper: deeper_b}) do
