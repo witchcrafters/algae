ea2f3acb5c2935afc15c42af62fca9b863c4136e
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-14 00:06:08 +0100

    :tada:

--- lib/algae/free/generator.ex
+++ lib/algae/free/generator.ex
@@ -1,25 +1,26 @@
-# alias Algae.Free.{Deep, Shallow}
+alias  Algae.Free.{Pure, Roll}
+import Algae.Free
 
-# defimpl TypeClass.Property.Generator, for: Shallow do
-#   def generate(_) do
-#     [1, 1.1, "", []]
-#     |> Enum.random()
-#     |> TypeClass.Property.Generator.generate()
-#     |> Shallow.new()
-#   end
-# end
+defimpl TypeClass.Property.Generator, for: Algae.Free.Pure do
+  def generate(_) do
+    [1, 1.1, "", []]
+    |> Enum.random()
+    |> TypeClass.Property.Generator.generate()
+    |> Pure.new()
+  end
+end
 
-# defimpl TypeClass.Property.Generator, for: Deep do
-#   def generate(_) do
-#     sample = Enum.random([1, 1.1, "", []])
+defimpl TypeClass.Property.Generator, for: Algae.Free.Roll do
+  def generate(_) do
+    sample = Enum.random([1, 1.1, "", []])
 
-#     a = TypeClass.Property.Generator.generate(sample)
-#     b = TypeClass.Property.Generator.generate(sample)
-#     c = TypeClass.Property.Generator.generate(sample)
+    a = TypeClass.Property.Generator.generate(sample)
+    b = TypeClass.Property.Generator.generate(sample)
+    c = TypeClass.Property.Generator.generate(sample)
 
-#     a
-#     |> Algae.Free.new()
-#     |> Algae.Free.layer(Algae.Free.new(b))
-#     |> Algae.Free.layer(Algae.Free.new(c))
-#   end
-# end
+    a
+    |> new()
+    |> layer(new(b))
+    |> layer(new(c))
+  end
+end
--- lib/algae/free/setoid.ex
+++ lib/algae/free/setoid.ex
@@ -1,36 +1,32 @@
-# alias  Algae.Free.{Deep, Shallow}
-# import TypeClass
+alias  Algae.Free.{Pure, Roll}
+alias  Witchcraft.Setoid
 
-# definst Witchcraft.Setoid, for: Algae.Free.Shallow do
-#   custom_generator(_) do
-#     1
-#     |> TypeClass.Property.Generator.generate()
-#     |> Algae.Free.new()
-#   end
+import Algae.Free
+import TypeClass
 
-#   def equivalent?(_, %Deep{}), do: false
+definst Witchcraft.Setoid, for: Algae.Free.Pure do
+  custom_generator(_) do
+    1
+    |> TypeClass.Property.Generator.generate()
+    |> Algae.Free.new()
+  end
 
-#   def equivalent?(%Shallow{shallow: a}, %Shallow{shallow: b}) do
-#     Witchcraft.Setoid.equivalent?(a, b)
-#   end
-# end
+  def equivalent?(_, %Roll{}), do: false
+  def equivalent?(%Pure{pure: a}, %Pure{pure: b}), do: Setoid.equivalent?(a, b)
+end
 
-# definst Witchcraft.Setoid, for: Algae.Free.Deep do
-#   custom_generator(_) do
-#     a = TypeClass.Property.Generator.generate(1)
-#     b = TypeClass.Property.Generator.generate(1)
-#     c = TypeClass.Property.Generator.generate(1)
+definst Witchcraft.Setoid, for: Algae.Free.Roll do
+  custom_generator(_) do
+    a = TypeClass.Property.Generator.generate(1)
+    b = TypeClass.Property.Generator.generate(1)
+    c = TypeClass.Property.Generator.generate(1)
 
-#     a
-#     |> Algae.Free.new()
-#     |> Algae.Free.layer(Algae.Free.new(b))
-#     |> Algae.Free.layer(Algae.Free.new(c))
-#   end
+    a
+    |> new()
+    |> layer(new(b))
+    |> layer(new(c))
+  end
 
-#   def equivalent?(_, %Shallow{}), do: false
-
-#   def equivalent?(%Deep{deep: deep_a, deeper: deeper_a}, %Deep{deep: deep_b, deeper: deeper_b}) do
-#     Witchcraft.Setoid.equivalent?(deep_a, deep_b)
-#     and Witchcraft.Setoid.equivalent?(deeper_a, deeper_b)
-#   end
-# end
+  def equivalent?(_, %Pure{}), do: false
+  def equivalent?(%Roll{roll: a}, %Roll{roll: b}), do: Setoid.equivalent?(a, b)
+end
