679971802e5c3f947d904364b4e465b505250d7b
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-14 00:40:29 +0100

    Free

---
+++ lib/algae/free/chain.ex
@@ -0,0 +1,17 @@
+alias  Algae.Free.{Pure, Roll}
+alias  Witchcraft.{Functor, Chain}
+
+import Algae.Free
+import TypeClass
+
+definst Witchcraft.Chain, for: Algae.Free.Pure do
+  def chain(%Pure{pure: pure}, link), do: link.(pure)
+end
+
+definst Witchcraft.Chain, for: Algae.Free.Roll do
+  def chain(%Roll{roll: rolled}, link) do
+    rolled
+    |> Functor.map(&Chain.chain(&1, link))
+    |> Roll.new()
+  end
+end
