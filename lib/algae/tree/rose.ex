ef3f39e2f1780aa1b25f83b0ab41e223f0b8f386
Brooklyn Zelenka <be.zelenka@gmail.com>
2017-08-14 01:55:54 +0100

    Specs

--- lib/algae/tree/rose.ex
+++ lib/algae/tree/rose.ex
@@ -6,12 +6,12 @@ defmodule Algae.Tree.Rose do
 
       %Algae.Tree.Rose{
         rose: 42,
-        tree: [
+        forrest: [
           %Algae.Tree.Rose{
             rose: "hi"
           },
           %Algae.Tree.Rose{
-            tree: [
+            forrest: [
               %Algae.Tree.Rose{
                 rose: 0.4
               }
@@ -36,9 +36,69 @@ defmodule Algae.Tree.Rose do
     forrest :: [t()]
   end
 
+  @doc """
+  Create a simple `Algae.Rose` tree, with an empty forrest and one rose.
+
+  ## Examples
+
+      iex> new(42)
+      %Algae.Tree.Rose{
+        rose: 42,
+        forrest: []
+      }
+
+  """
+  @spec new(rose()) :: t()
   def new(rose), do: %Rose{rose: rose, forrest: []}
 
+  @doc """
+  Create an `Algae.Rose` tree, passing a forrest and a rose.
+
+  ## Examples
+
+      iex> new(42, [new(55), new(14)])
+      %Algae.Tree.Rose{
+        rose: 42,
+        forrest: [
+          %Algae.Tree.Rose{rose: 55},
+          %Algae.Tree.Rose{rose: 14}
+        ]
+      }
+
+  """
+  @spec new(rose(), forrest()) :: t()
   def new(rose, forrest), do: %Rose{rose: rose, forrest: forrest}
 
+  @doc """
+  Wrap another tree around an existing one, relegating it to the forrest.
+
+  ## Examples
+
+      iex> 55
+      ...> |> new()
+      ...> |> layer(42)
+      ...> |> layer(99)
+      ...> |> layer(6)
+      %Algae.Tree.Rose{
+        rose: 6,
+        forrest: [
+          %Algae.Tree.Rose{
+            rose: 99,
+            forrest: [
+              %Algae.Tree.Rose{
+                rose: 42,
+                forrest: [
+                  %Algae.Tree.Rose{
+                    rose: 55
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+
+  """
+  @spec layer(t(), rose()) :: t()
   def layer(tree, rose), do: %Rose{rose: rose, forrest: [tree]}
 end
